c06944c8ce1232680d627df147b874d49d541c5f
goldmund <matthewtzito@gmail.com>
2021-03-10 08:39:36 -0800

    chore: update sh scripts

--- scripts/docker_prune.sh
+++ scripts/docker_prune.sh
@@ -3,11 +3,12 @@
 # super dirty docker prune
 
 containers=($(docker ps -a | awk '{ print $1 }'))
+cmds=("image" "volume" "network")
 
 for container in "${containers[@]}"; do
   docker rm "$container" 2>/dev/null
 done
 
-docker image prune 2>/dev/null
-docker volume prune 2>/dev/null
-docker network prune 2>/dev/null
+for cmd in "${cmds[@]}"; do
+  docker "$cmd" prune 2>/dev/null
+done
